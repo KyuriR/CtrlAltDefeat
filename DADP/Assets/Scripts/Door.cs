9c430230dd5e727920d3eb63575553ccf45f8fad
KyuriR <kyuri.reddy@gmail.com>
2024-08-15 14:09:42 +0200

    andy consult

---
+++ DADP/Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door: MonoBehaviour
+{
+    public Animator animator;
+    private bool isDoorOpen = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("AnimateDoor");
+            Debug.Log("open door");
+        }
+    }
+
+    //void OpenDoor()
+    //{
+    //    animator.SetTrigger("Open");
+    //    isDoorOpen = true;
+    //}
+
+    //void CloseDoor()
+    //{
+    //    animator.SetTrigger("Close");
+    //    isDoorOpen = false;
+    //}
+}
