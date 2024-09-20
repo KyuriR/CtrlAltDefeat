d82e84f5b0c23775d38b27f6eaf5bb347e79974c
KyuriR <kyuri.reddy@gmail.com>
2024-09-20 13:44:32 +0200

    Kick door animation
    doesn't work yet

---
+++ DADP/Assets/Scripts/DoorKick.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorKick : MonoBehaviour
+{
+    public Animator doorAnimator;
+    public string kickTrigger = "Kick";
+    private bool isPlayerInRange = false;
+
+    void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.K))
+        {
+            KickDoor();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the player enters the collider range
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the player exits the collider range
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+        }
+    }
+
+    private void KickDoor()
+    {
+        // Play the kick animation
+        if (doorAnimator != null)
+        {
+            doorAnimator.SetTrigger("Kick");
+        }
+    }
+}
