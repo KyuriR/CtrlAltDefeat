60b864db11fc1f89a10b3d0ca341d4793db707e3
TaiK1505 <taikistnasami@gmail.com>
2024-09-20 19:44:15 +0200

    fixed stair doors

--- DADP/Assets/Scripts/Door.cs
+++ DADP/Assets/Scripts/Door.cs
@@ -7,20 +7,49 @@ public class Door : MonoBehaviour
 {
     public Animator animator;
     private bool isDoorOpen = false;
+    private bool isPlayerInRange;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.O))
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
         {
             if (isDoorOpen == false)
             {
-                animator.SetTrigger("AnimateDoor"); //open the door
-                Debug.Log("open door");
+                OpenStairDoor();
             }
             else
             {
-                animator.SetTrigger("AnimateDoor"); //close the door
+                CloseStairDoor();
             }
+        }
+    }
+
+    void OpenStairDoor()
+    {
+        animator.SetTrigger("AnimateDoor"); //open the door
+        Debug.Log("open door");
+    }
+
+    void CloseStairDoor()
+    {
+        animator.SetTrigger("AnimateDoor"); //close the door
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the player enters the collider range
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the player exits the collider range
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
         }
     }
 }
